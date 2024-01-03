358aa6a0286fb812979a11daf487d28febdbe076
John Assebe <johnassebe@gmail.com>
2024-01-04 00:38:28 +0300

    chore(regex): find more than the first match :sparkles:

---
+++ day3/moreMatch.js
@@ -0,0 +1,7 @@
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi;
+let result = twinkleStar.match(starRegex);
+console.log(result);
+
+// global flag (g) To search or extract a pattern more than once,
+// ignore case (i) to avoid case sensativity in looking
