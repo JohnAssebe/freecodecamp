7d943ad6e386c916153ff182ddf4501981ee26d5
John Assebe <johnassebe@gmail.com>
2024-01-05 02:16:40 +0300

    chore(regex): all vowels in the sentence :sparkles:

--- day3/moreMatch.js
+++ day3/moreMatch.js
@@ -2,6 +2,5 @@ let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /twinkle/gi;
 let result = twinkleStar.match(starRegex);
 console.log(result);
-
 // global flag (g) To search or extract a pattern more than once,
 // ignore case (i) to avoid case sensativity in looking
