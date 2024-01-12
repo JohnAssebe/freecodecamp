7dfe53200ca5f59a0646313b2b80d0a5b9a76dff
John Assebe <johnassebe@gmail.com>
2024-01-12 22:28:40 +0300

    chore(debugging): methods of JS debugging :sparkles:

---
+++ day4/consoles.js
@@ -0,0 +1,19 @@
+console.log("john");
+console.error("john");
+console.warn("john");
+// console.clear();
+
+// one method of debugging is using console types in different breaklines 
+
+// check the typeof variables is the other method of debugging 
+console.log(typeof "");
+console.log(typeof 0);
+console.log(typeof []);
+console.log(typeof {});
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+console.log(typeof seven);
+console.log(typeof three);
+
